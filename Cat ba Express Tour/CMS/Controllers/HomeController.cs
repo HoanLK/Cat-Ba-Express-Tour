0e7aa0cd4ea6429d2612b7fd5f0463a8a498351c
llathanh1108 <thanh1108@gmail.com>
2017-02-09 10:01:20 +0700

    Init

---
+++ Cat ba Express Tour/CMS/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private HoaBanTravelEntities db = new HoaBanTravelEntities();
+        [Route]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("du-lich-trong-nuoc")]
+        public ActionResult TrongNuoc()
+        {
+            return View();
+        }
+
+        [Route("du-lich-nuoc-ngoai")]
+        public ActionResult NuocNgoai()
+        {
+            return View();
+        }
+
+        [Route("dich-vu-du-lich")]
+        public ActionResult DichVuDuLich()
+        {
+            return View();
+        }
+
+        [Route("lien-he")]
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+
+        public ActionResult DanhSachTour()
+        {
+            return View();
+        }
+
+        public ActionResult DiaDiemDep()
+        {
+            return View();
+        }
+        public ActionResult TinTuc()
+        {
+            return View();
+        }
+    }
+}
